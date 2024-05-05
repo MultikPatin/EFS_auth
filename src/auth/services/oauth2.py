28ecaec4074f2a64ffbc9c9a1475318d3b6b219d
vlad <threeeyesvl@gmail.com>
2024-05-05 22:38:11 +0300

    Рефакторинг

--- src/auth/services/oauth2.py
+++ src/auth/services/oauth2.py
@@ -1,9 +1,6 @@
 from functools import lru_cache
 from http import HTTPStatus
 
-from authlib.integrations.httpx_client import AsyncOAuth2Client
-from authlib.jose import jwt
-from authlib.oidc.core import CodeIDToken
 from fastapi import Depends, HTTPException, Request
 
 # from starlette.requests import Request
@@ -14,64 +11,45 @@ from src.core.db.repositories.login_history import (
     get_login_history_repository,
 )
 from src.core.db.repositories.user import UserRepository, get_user_repository
-
-scope = "openid email profile"
-redirect_uri = "https://d6a9-91-108-28-17.ngrok-free.app/auth/v1/oauth2/auth"
-google_config = settings.google_config
-
-client = AsyncOAuth2Client(
-    settings.google_client_id,
-    settings.google_client_secret.get_secret_value(),
-    scope=scope,
-    redirect_uri=redirect_uri,
-    prompt="consent",
-)
+from src.core.oauth_clients.google import OauthGoogle, get_google
 
 
 class OAuth2Service:
     def __init__(
         self,
         cache: RedisCache,
+        oauth_client: OauthGoogle,
         user_repository: UserRepository,
         history_repository: LoginHistoryRepository,
     ):
         self._cache = cache
+        self._oauth_client = oauth_client
         self._user_repository = user_repository
         self._history_repository = history_repository
-        self._client = client
 
     async def get_authorization_url(self, request: Request) -> str:
-        authorization_endpoint = google_config.get("authorization_endpoint")
-        authorization_url, _ = self._client.create_authorization_url(
-            authorization_endpoint,
-            state=settings.google_state.get_secret_value(),
-        )
-        valid_authorization_url = authorization_url + "&access_type=offline"
-
-        return valid_authorization_url
+        return await self._oauth_client.create_authorization_url()
 
     async def auth_via_google(self, request: Request):
-        authorization_response = str(request.url)
-        access_token_endpoint = google_config.get("token_endpoint")
         if (
             request.query_params.get("state")
-            != settings.google_state.get_secret_value()
+            != settings.google.google_state.get_secret_value()
         ):
             raise HTTPException(
                 status_code=HTTPStatus.UNAUTHORIZED,
                 detail="Invalid state parameter.",
             )
-        response = await self._client.fetch_token(
-            access_token_endpoint, authorization_response=authorization_response
-        )
+
+        authorization_response = str(request.url)
+        response = await self._oauth_client.fetch_token(authorization_response)
+
         access_token = response.get("access_token")
         refresh_token = response.get("refresh_token")
         id_token = response.get("id_token")
-        keys_response = await self._client.get(google_config.get("jwks_uri"))
-        keys = keys_response.json()
-        claims = jwt.decode(id_token, keys, claims_cls=CodeIDToken)
-        claims.validate()
 
+        claims = await self._oauth_client.get_claims(id_token)
+
+        print(f"\n\n{type(id_token)}\n\n")
         print(f"\n\n{access_token=}\n\n")
         print(f"\n\n{refresh_token=}\n\n")
         print(f"\n\n{id_token=}\n\n")
@@ -82,9 +60,12 @@ class OAuth2Service:
 @lru_cache
 def get_oauth2_service(
     cache: RedisCache = Depends(get_redis),
+    oauth_client: OauthGoogle = Depends(get_google),
     user_repository: UserRepository = Depends(get_user_repository),
     history_repository: LoginHistoryRepository = Depends(
         get_login_history_repository
     ),
 ) -> OAuth2Service:
-    return OAuth2Service(cache, user_repository, history_repository)
+    return OAuth2Service(
+        cache, oauth_client, user_repository, history_repository
+    )
